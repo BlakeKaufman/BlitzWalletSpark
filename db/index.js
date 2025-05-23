27adad8d39eba1c9c349e2333f60bc003045b97f
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-23 16:07:49 -0400

    fixing unimported method

--- db/index.js
+++ db/index.js
@@ -14,6 +14,7 @@ import {
   setDoc,
   limit,
   addDoc,
+  writeBatch,
 } from '@react-native-firebase/firestore';
 import {getLocalStorageItem, setLocalStorageItem} from '../app/functions';
 import {
