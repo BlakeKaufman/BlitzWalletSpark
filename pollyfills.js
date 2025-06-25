bb145e1bdc3d5818fe441852fb4ce986ef297542
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-25 08:24:07 -0400

    updating spark + fixing ios build issue

--- pollyfills.js
+++ pollyfills.js
@@ -7,7 +7,8 @@ import '@azure/core-asynciterator-polyfill';
 import {Buffer} from '@craftzdog/react-native-buffer';
 global.Buffer = Buffer;
 
-import 'text-encoding-polyfill';
+// import 'text-encoding-polyfill';
+import 'text-encoding'; // needed for spark
 
 // Process polyfill
 global.process = global.process || {};
