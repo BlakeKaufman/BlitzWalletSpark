3bd0b4f8af9f017219030948b935e7e6fc471775
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-25 11:15:03 -0400

    remove initial liquid connection

--- app/screens/inAccount/login.js
+++ app/screens/inAccount/login.js
@@ -1,16 +1,7 @@
-import {useEffect} from 'react';
 import PinPage from '../../components/admin/loginComponents/pinPage';
 import {GlobalThemeView} from '../../functions/CustomElements';
-import {useLiquidEvent} from '../../../context-store/liquidEventContext';
-import connectToLiquidNode from '../../functions/connectToLiquid';
 
 export default function AdminLogin() {
-  const {onLiquidBreezEvent} = useLiquidEvent();
-
-  useEffect(() => {
-    connectToLiquidNode(onLiquidBreezEvent);
-  }, []);
-
   return (
     <GlobalThemeView useStandardWidth={true}>
       <PinPage />
